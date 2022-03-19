35d8952fbd9721a42a96382ec500f820ad56aee0
Kris Watson <kris@computestacks.com>
2022-03-19 10:13:50 +0100

    Update to appid

--- app/services/exception_alert_service.rb
+++ app/services/exception_alert_service.rb
@@ -14,21 +14,23 @@ class ExceptionAlertService
     if SENTRY_CONFIGURED
       Raven::Context.clear!
       Raven.tags_context(event_code: event_code)
-      Raven.extra_context(
-        extra: {
-          provider: ENV['APP_ID']
-        }
-      )
-      if user && Feature.check('exception_user_info')
-        Raven.user_context(
-          id: "#{ENV['APP_ID']}-#{user.id}",
-          email: user.email,
-          ip_address: user.last_request_ip.to_s
+      unless CS_APP_ID.blank?
+        Raven.extra_context(
+          extra: {
+            provider: CS_APP_ID
+          }
         )
-      elsif user
-        Raven.user_context(
-          id: "#{ENV['APP_ID']}-#{user.id}"
-        )
+        if user && Feature.check('exception_user_info')
+          Raven.user_context(
+            id: "#{CS_APP_ID}-#{user.id}",
+            email: user.email,
+            ip_address: user.last_request_ip.to_s
+          )
+        elsif user
+          Raven.user_context(
+            id: "#{CS_APP_ID}-#{user.id}"
+          )
+        end
       end
       Raven.capture_exception(e)
       Raven::Context.clear!
--- config/initializers/cloudportal.rb
+++ config/initializers/cloudportal.rb
@@ -19,6 +19,19 @@ NS_LIST = if Rails.env.production?
 
 PORTAL_HTTP_SCHEME = Rails.env.production? ? "https" : "http"
 
+begin
+  # Since users can set this during install, lets try to guess some possible commons words they may use to identify their installation.
+  # We really want a unique name, and if they're just testing with 'dev' or 'demo', we want to generate a unique name.
+  app_id_exclude = %w(default dev development local localhost demo trial test tester testing cs cstacks cmptstks computestacks docker containers poc)
+  CS_APP_ID = if ENV['APP_ID'].blank? || app_id_exclude.include?(ENV['APP_ID'])
+                Digest::SHA256.hexdigest(ENV['SECRET_KEY_BASE'])[-8...-1]
+              else
+                ENV['APP_ID']
+              end
+rescue
+  CS_APP_ID = ""
+end
+
 BYTE_TO_GB = Numeric::GIGABYTE.to_f # old: 1.074e+9 # 1024, not 1000
 KILOBYTE_TO_GB = Numeric::GIGABYTE / 1024.0 # old: 1.049e+6 # 1024, not 1000
 
