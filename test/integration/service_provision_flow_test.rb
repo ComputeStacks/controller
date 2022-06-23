86f1aff96de4c5e10b8e32a74d0cd3fc409bd125
Kris Watson <kris@computestacks.com>
2022-06-23 13:43:53 -0700

    test for mounting shared volume

--- test/integration/service_provision_flow_test.rb
+++ test/integration/service_provision_flow_test.rb
@@ -84,6 +84,79 @@ class ServiceProvisionFlowTest < ActionDispatch::IntegrationTest
       refute_nil service.package
     end
 
+  end
+
+  test 'can provision nginx with mountable volume' do
+
+    # Build order session
+    order_session = OrderSession.new users(:admin)
+    order_session.add_image container_images(:nginx_shared)
+    order_session.project.name = 'Test nginx Order'
+
+    order_session.images.each do |image|
+      image[:package_id] = products(:containersmall).id
+    end
+
+    assert_includes order_session.images.collect {|i| i[:container_id]}, container_images(:nginx_shared).id
+    # Ensure it added our required dependency
+    assert_includes order_session.images.collect {|i| i[:container_id]}, container_images(:nginx).id
+
+    # Create Order
+    audit = Audit.create!(
+      user: users(:admin),
+      ip_addr: '127.0.0.1',
+      event: 'created'
+    )
+    build_order = BuildOrderService.new(audit, order_session.to_order)
+    build_order.process_order = false
+
+    prep_order_success = build_order.perform
+    puts build_order.errors.join(" ")
+    assert prep_order_success
+
+    order = build_order.order
+    assert_kind_of Order, order
+
+    # Process Order
+    event = EventLog.create!(
+      locale: 'order.provision',
+      event_code: '0a3af01a3384fa10',
+      audit: audit,
+      status: 'pending'
+    )
+    order.current_event = event
+    order_process = ProcessOrderService.new(order)
+
+    Sidekiq::Testing.inline! do
+      order_prov_success = order_process.perform
+      puts order_process.errors.join(" ") unless order_process.errors.empty?
+      assert order_prov_success
+    end
+
+    project = order.deployment
+
+    nginx = project.services.where( container_images: { name: 'nginx' } ).joins(:container_image).first
+    nginx_mount = project.services.where( container_images: { name: 'nginx_shared' } ).joins(:container_image).first
+
+    # Ensure we only created a single volume
+    assert_equal nginx.volumes.first, nginx_mount.volumes.first
+
+    refute_nil project
+
+    refute_empty project.services
+    refute_empty project.deployed_containers
+    refute_empty project.domains
+
+    # Test Container Configuration
+    project.deployed_containers.each do |c|
+      assert_kind_of Hash, c.runtime_config
+    end
+
+    # Ensure subscription was setup correctly
+    project.services.each do |service|
+      refute_nil service.package
+    end
 
   end
+
 end
