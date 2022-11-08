2555047bc7372f8a44825103b0163b8bc2933a0a
Kris Watson <kris@computestacks.com>
2022-11-07 19:22:01 -0800

    Push image upgrade migration to end of list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_035927) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_032120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
