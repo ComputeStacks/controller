febee486cfc2464fdb9e666f538836524e02e97d
Kris Watson <kris@computestacks.com>
2024-04-19 15:31:51 -0700

    Fix broken i18n loading

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ require './lib/core_ext/string'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Wipe out default Rails & gem i18n.
-I18n.load_path = Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+# Wipe out default gem i18n.
+I18n.load_path = I18n.load_path.delete_if { |i| i =~ /gems/ }
+
+# I18n.load_path = Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+# I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
